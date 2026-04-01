7e72a257693fbfd3e356cdb695d6cbc38ba76e32
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-01 13:45:41 -0300

    Terceira modificação

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ print('Hello, World!')
 print('This is a simple Python program.')
 print('It demonstrates basic syntax and output.')
 print('You can run this code to see the output in your console.')
-
+print('terceira modificação')
