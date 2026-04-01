48c0b37c9a9f6d3ff89aacaca180648d13ae9669
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-01 13:31:49 -0300

    Segunda modificação no arquivo main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 print('Hello, World!')
+print('This is a simple Python program.')
+print('It demonstrates basic syntax and output.')
+print('You can run this code to see the output in your console.')
 
